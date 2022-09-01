84234e68a3c861e023bd85ab755fab373fcc9259
DivyanshSareen <sareendivyansh@gmail.com>
2022-09-01 19:14:38 +0530

    chore: added mock data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import assetGrid from "./components/AssetGrid.js";
 
 const asset_grid = document.getElementsByClassName("asset-grid")[0];
-console.log(assetGrid());
 
 asset_grid.innerHTML = assetGrid();
