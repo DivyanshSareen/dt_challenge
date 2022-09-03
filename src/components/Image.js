53cd33193e3ee500f6ca440b25f0c33e23919fa2
DivyanshSareen <sareendivyansh@gmail.com>
2022-09-03 20:56:00 +0530

    feat: added Image and Video components

---
+++ src/components/Image.js
@@ -0,0 +1,17 @@
+const Image = () => {
+  return `<div class="asset">
+                <h3 class="asset-head">${data.asset_title}</h3>
+                <div class="drive-embed youtube">
+                    <iframe width="auto" src="https://giphy.com/embed/IzjhI7ggjDlEnMxZMu" frameBorder="0"
+                        class="giphy-embed" allowFullScreen></iframe>
+                </div>
+                <div class="asset-description hidden">
+                    ${data.asset_description}
+                </div>
+                <div class="asset-expand">
+                    <img src="./src/assets/angle-down-solid.svg">
+                </div>
+            </div>`;
+};
+
+export default Image;
